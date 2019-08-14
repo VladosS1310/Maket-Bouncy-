2ded542941a483c560a71fccb2752af0f41fc0eb
VladosS1310 <vladik.prindyuk007@gmail.com>
2019-08-14 22:32:19 +0300

    add Maket Bouncy

---
+++ script.js
@@ -0,0 +1,7 @@
+
+	var name = document.getElementById("name");
+ 	var email = document.getElementById("email");
+ 	var password = document.getElementById("password");
+
+ 	var patern_name = /[a-z]/;
+ 	patern_name.test(name.value());
